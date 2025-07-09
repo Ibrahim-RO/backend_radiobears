643df8818bdf4cc5cbcaa6dbd12fb8a27c1d2dc7
Ibrahim <ibra.rodriguez.olaya@gmail.com>
2025-07-09 11:20:03 -0600

    Add origin for cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,7 +2,7 @@ import { CorsOptions } from 'cors'
 
 export const corsConfig: CorsOptions = {
     origin: function(origin, callback){
-        if(origin === process.env.FRONTEND_URL) {
+        if(origin === process.env.FRONTEND_URL || origin === process.env.FRONTEND_URL_2) {
             callback(null, true)
         } else {
             callback(new Error('Error de CORS'))
