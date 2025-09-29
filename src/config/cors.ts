6d428bfd68c2874f8e3c479ffe4c35a0883bc5a1
ibrahim <ibra.rodriguez.olaya@gmail.com>
2025-09-28 18:57:17 -0600

    fix: Update CORS whitelist to include additional frontend URL

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,7 +2,7 @@ import { CorsOptions } from 'cors'
 
 export const corsConfig: CorsOptions = {
     origin: function(origin, callback){
-        const whiteList = [process.env.FRONTEND_URL, process.env.FRONTEND_URL_2]
+        const whiteList = [process.env.FRONTEND_URL, process.env.FRONTEND_URL_2, process.env.FRONTEND_URL_3]
 
         if(process.argv[2] === '--api'){
             whiteList.push(undefined)
